02faa827b1a4473ea19b89e25fc3042e233486a2
Fez Zaman <fzaman@oswego.edu>
2018-04-08 10:01:57 -0400

    Added background alpha dynamics and vertex shape modification.

--- dish.outerport/17-nov19/sketch.js
+++ dish.outerport/17-nov19/sketch.js
@@ -12,8 +12,28 @@ let b = 255
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	background(0)
 }
 
 function draw() {
+	let alpha = map(delta, 0, width, 0, 255)
+	background(0, alpha)
+	delta += speed * dir
+	stroke(r,g,b)
+	beginShape()
 
+	for (var i = 0; i < 2; i++) {
+		x = (width/2) - delta
+		y = (height/2) - (i * delta)
+		vertex(x,y)
+	}
+
+	endShape()
+
+	if (x > width || x < 0) {
+		dir *=-1
+		r = random(0,255)
+		g = random(0,255)
+		b = random(0,255)
+	}
 }

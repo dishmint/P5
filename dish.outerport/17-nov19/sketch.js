c09107f596bfe61b61c4e4f67628d81c88ac3175
Fez Zaman <fzaman@oswego.edu>
2018-04-08 09:58:29 -0400

    Added initial variables

---
+++ dish.outerport/17-nov19/sketch.js
@@ -0,0 +1,19 @@
+let speed = 1
+let dir = 1
+
+let delta = 0
+
+let x
+let y
+
+let r = 255
+let g = 255
+let b = 255
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+
+}

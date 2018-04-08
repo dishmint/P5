57237cc82908ca78cf4a77fdd8c77e0399f0ea3d
Fez Zaman <fzaman@oswego.edu>
2018-04-08 10:31:31 -0400

    Constrained Node position to the canvas | simple physics => pos+vel+acc

--- dish.outerport/17-1216.attractorNet/scripts/Node.js
+++ dish.outerport/17-1216.attractorNet/scripts/Node.js
@@ -4,4 +4,20 @@ class Node {
     this.vel = p5.Vector.random2D()
     this.acc = createVector()
   }
+
+  update() {
+    this.pos.x = constrain(this.pos.x, 0, width)
+    this.pos.y = constrain(this.pos.y, 0, height)
+
+    this.pos.add(this.vel)
+    this.vel.add(this.acc)
+    this.acc.mult(0)
+  }
+
+  attracted(target) {
+    let trg = createVector(target.pos.x, target.pos.y)
+    let force = p5.Vector.sub(trg, this.pos)
+    let d = force.mag()
+    d = constrain(d, 5, 25)
+  }
 }

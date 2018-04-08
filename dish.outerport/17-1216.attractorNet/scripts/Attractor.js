8ef8c6e0798e5446de7168a6acf751990dd3479b
Fez Zaman <fzaman@oswego.edu>
2018-04-08 10:22:33 -0400

    Added Attractor Class :c position, state
    :c => constructor

---
+++ dish.outerport/17-1216.attractorNet/scripts/Attractor.js
@@ -0,0 +1,13 @@
+/*
+ * Attractor.js
+ * created by faizonzaman
+ * on Sun, Apr 8th 2018 at 10:19 am
+ * in Pittsford, US
+ */
+
+class Attractor{
+  constructor(x,y,s){
+    this.pos = createVector(x,y)
+    this.state = s
+  }
+}

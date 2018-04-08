be495937b30f47e885876ca6f19ec8c6226858e0
Fez Zaman <fzaman@oswego.edu>
2018-04-08 10:55:16 -0400

    Added array for nodes and an attractor grid

--- dish.outerport/17-1216.attractorNet/sketch.js
+++ dish.outerport/17-1216.attractorNet/sketch.js
@@ -1,5 +1,15 @@
+// An Array for the Nodes
+let nodes = []
+
+// Empty var for attractor grid
+let g
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+
+	for(let i=0; i < 5; i++){
+		nodes.push(new Node(width/2, height/2))
+	}
+	g = new AttractorGrid(2,2)
 }
 
 function draw() {

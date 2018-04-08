df2c9376b93b28e4e6f6bec88cedf2fb4e78fc68
Fez Zaman <fzaman@oswego.edu>
2018-04-08 10:36:33 -0400

    Node displayed as white ellipse. connect(nd) draws line between 2 nodes

--- dish.outerport/17-1216.attractorNet/scripts/Node.js
+++ dish.outerport/17-1216.attractorNet/scripts/Node.js
@@ -19,5 +19,32 @@ class Node {
     let force = p5.Vector.sub(trg, this.pos)
     let d = force.mag()
     d = constrain(d, 5, 25)
+
+    let strength
+    if (target.state == 1) {
+      strength = 1
+    } else {
+      force.mult(-1)
+    }
+
+    force.setMag(strength)
+    this.acc.add(force)
+  }
+
+  show(){
+    push()
+    stroke(255)
+    ellipse(this.pos.x, this.pos.y, 20)
+    pop()
+  }
+
+  connect(nd){
+    push()
+    stroke(255)
+    this.update()
+    this.show()
+    nd.update()
+    nd.show()
+    line(this.pos.x, this.pos.y, nd.pos.x, nd.pos.y)
   }
 }

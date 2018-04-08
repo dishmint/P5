893e0dde426284c4f3bfe10bf0baa57a6f7fb31c
Fez Zaman <fzaman@oswego.edu>
2018-04-08 11:46:08 -0400

    Updated the sketch file

--- dish.outerport/17-1215.attractors/sketch.js
+++ dish.outerport/17-1215.attractors/sketch.js
@@ -1,7 +1,27 @@
+let g
+let gs
+
+let scale
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	ellipseMode(CENTER)
+
+	g = new Grid(4,4)
+
+	gs = new GridSpace(50,50,g,2)
+	gs.fillMatrix()
+	gs.show()
 }
 
 function draw() {
+	background(0)
 
+	gs.fillMatrix()
+	gs.show()
+	g.show()
+
+	if(frameCount % 15 == 0){
+		g.fillMatrix()
+	}
 }

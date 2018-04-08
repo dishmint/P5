c5036c76ca17f78dcf2f8613d365145941795e76
Fez Zaman <fzaman@oswego.edu>
2018-04-08 09:28:40 -0400

    Setup an array of "squares".

--- dish.daily/40818/sketch.js
+++ dish.daily/40818/sketch.js
@@ -1,7 +1,16 @@
+// Lets do something with rotating squares!
+
+let squares
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	pixelDensity(displayDensity())
+
+	let size = 1
+	squares = new Array(size)
 }
 
 function draw() {
+
 
 }

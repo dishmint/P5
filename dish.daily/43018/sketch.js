95aade03a3b3857e596216ed675e31f91a3f164e
Fez Zaman <fzaman@oswego.edu>
2018-05-03 02:26:28 -0400

    Ran out of time

--- dish.daily/43018/sketch.js
+++ dish.daily/43018/sketch.js
@@ -1,7 +1,22 @@
+let n = 0
+let dir = 1
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	background(0)
+	rectMode(CENTER)
+	angleMode(DEGREES)
 }
 
 function draw() {
+stroke(255)
+noFill()
+translate(width/2, height/2)
+rotate(n)
+translate(-width/2, -height/2)
+rect(width/2, height/2, 200,200)
 
+	n+= dir
+if (n > 360) {
+	dir *= -1
+}
 }

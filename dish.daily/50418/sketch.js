9c2275413aa2ace844f3c49ec6bae8d8d737c528
Fez Zaman <fzaman@oswego.edu>
2018-05-05 01:31:39 -0400

    Rotating Circle

--- dish.daily/50418/sketch.js
+++ dish.daily/50418/sketch.js
@@ -1,7 +1,23 @@
+let change = 0
+let dir = 1
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	ellipseMode(CENTER)
 }
 
 function draw() {
+	background(0)
 
+	stroke('tomato')
+	noFill()
+
+	change += dir
+	ellipse(width/2, height/2, change, 40)
+
+	if (change > 40 || change < 0) {
+		dir *= -1
+
+
+	}
 }

7fc1b371c65e987590979d7881c2e8357789f300
Fez Zaman <fzaman@oswego.edu>
2018-08-17 11:41:17 -0400

    updated 3rd c daily

--- dish.daily/p5.daily.032318_3/sketch.js
+++ dish.daily/p5.daily.032318_3/sketch.js
@@ -1,16 +1,21 @@
-// File: 32318
+// File: 032318_3
+
 let change = 0
 let direction = 1
+let angle = 0
+
 function setup() {
   createCanvas(400, 400)
   background(0)
   pixelDensity(displayDensity())
 
+  angleMode(DEGREES)
   rectMode(CENTER)
+  textAlign(CENTER)
 }
 
 function draw() {
-  background(0, 20)
+  background(0, 10)
   // background(0,50)
 
   // Connections
@@ -25,13 +30,22 @@ function draw() {
   pop()
 
   push()
-  // stroke(255, 165, 0)
+  fill(255, 165, 0)
   // background(0,30)
+  if (frameCount % 15 == 0) {
+    // text("Framecount: "+frameCount, width/2, height/6)
+    translate(width/2, height/2)
+    rotate(1.5*angle)
+    translate(-width/2, -height/2)
+  }
+
+  // stroke(255,0,0)
   stroke(30, 144, 255)
   line(width/2, height/2, change / 2, change / 2) /*Top Left*/
+  // stroke(255)
   line(width/2, height/2, width - change / 2, change / 2) /*Top Right*/
-  line(width/2, height, width - change / 2, height - change / 2) /*Bottom Right*/
-  line(width/2, height, change / 2, height - change / 2) /*Bottom Left*/
+  line(width/2, height/2, width - change / 2, height - change / 2) /*Bottom Right*/
+  line(width/2, height/2, change / 2, height - change / 2) /*Bottom Left*/
   pop()
 
   // Red Rectangle
@@ -54,5 +68,7 @@ function draw() {
   if (change > width || change < 0 || change > height) {
     direction *= -1
   }
+
+  angle ++
 
 }

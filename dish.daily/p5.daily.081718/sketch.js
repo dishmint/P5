41c6da87a28134f4c2ac94d305e96b4ef8f6c880
Fez Zaman <fzaman@oswego.edu>
2018-08-17 13:38:37 -0400

    Daily : Aug 17 2018

---
+++ dish.daily/p5.daily.081718/sketch.js
@@ -0,0 +1,26 @@
+// File: 081718
+
+let x = 0
+
+function setup(){
+  createCanvas(400,400)
+  background(0)
+  pixelDensity(displayDensity())
+  rectMode(CENTER)
+}
+
+
+function draw(){
+  background(0, 25)
+  noStroke()
+  strokeWeight(3 * cos(x))
+  fill('lime')
+  rect(width/2, height/2, map(cos(x), -1, 1, 10, width/2),map(sin(x), -1, 1, 10, width/2))
+  // rect(width/2, height/2, 50 * cos(x),50 * sin(x))
+  
+  x ++
+  
+  if (x > width) {
+    x = 0
+  }
+}

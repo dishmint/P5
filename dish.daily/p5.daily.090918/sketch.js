b393064636e4b225e7a724c16fbad0d53296f032
Fez Zaman <fzaman@oswego.edu>
2018-09-09 20:03:48 -0400

    Daily : September 9 2018

---
+++ dish.daily/p5.daily.090918/sketch.js
@@ -0,0 +1,50 @@
+// File: 090918
+
+function setup(){
+  createCanvas(400,400)
+  background(0)
+  pixelDensity(displayDensity())
+}
+
+
+function draw(){
+  background(0,5)
+  stroke(255)
+  push()
+  translate(width/2, height/2)
+  // rotate(5*frameCount)
+  // rotate(4*frameCount)
+  // rotate(3*frameCount)
+  rotate(2*frameCount)
+  let x1 = map(cos(frameCount * 0.01), -1, 1, 0, width)
+  let y1 = map(sin(frameCount * 0.01), -1, 1, 0, height)
+  let x2 = map(cos(frameCount * 0.01), -1, 1, 0, width)
+  let y2 = map(sin(frameCount * 0.01), -1, 1, 0, height)
+  let x3 = map(cos(frameCount * 0.01), -1, 1, 0, width)
+  let y3 = map(sin(frameCount * 0.01), -1, 1, 0, height)
+  let x4 = map(cos(frameCount * 0.01), -1, 1, 0, width)
+  let y4 = map(sin(frameCount * 0.01), -1, 1, 0, height)
+  
+  stroke(255,0,0,64)
+  line(x1/3, y1/3, x1/10, y1/10)
+  stroke(255,0,0,64)
+  line(x2/3, y2/3, x2/10, y2/10)
+  stroke(0,255,0,64)
+  line(x3/3, y3/3, x3/10, y3/10)
+  stroke(255,0,255,64)
+  line(x4/3, y4/3, x4/10, y4/10)
+  stroke(255)
+  quad(
+    x1/3,y1/3,
+    x2/3,y2/3,
+    x3/3,y3/3,
+    x4/3,y4/3)
+
+  quad(
+    x1/10,y1/10,
+    x2/10,y2/10,
+    x3/10,y3/10,
+    x4/10,y4/10)
+    
+  pop()
+}

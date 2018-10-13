a32670d4c6ff639c0fada8efcae668a5b324c71c
Fez Zaman <fzaman@oswego.edu>
2018-10-13 18:10:18 -0400

    Daily : October 09 2018

---
+++ dish.daily/p5.daily.100918/sketch.js
@@ -0,0 +1,31 @@
+// File: 100918
+
+function setup(){
+  createCanvas(400,400)
+  background(0)
+  pixelDensity(displayDensity())
+  rectMode(CENTER)
+}
+
+
+function draw(){
+  background(0,10)
+
+  let r = random(0, (width/2))
+  push()
+  translate(width/2, height/2)
+  rotate(frameCount)
+  translate(-width/2, -height/2)
+  
+  beginShape()
+  for (var i = 0; i < 10; i++) {
+    stroke(255)
+    strokeWeight(4)
+    line(r,height/2,(r + (width/2)),height/2)
+    stroke(0)
+    strokeWeight(10)
+    rect(width/2, height/2, 50, 50)
+  }
+  endShape(CLOSE)
+  pop()
+}

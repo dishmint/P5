f4de8cf82e4ad32bf3fb80e76a0c307bd80ff9bf
Fez Zaman <fzaman@oswego.edu>
2018-10-15 19:20:23 -0400

    Daily : October 15 2018

---
+++ dish.daily/p5.daily.101518/sketch.js
@@ -0,0 +1,50 @@
+// File: 101518
+
+let x1,y1,x2,y2
+
+let val = 1
+
+function setup(){
+  createCanvas(400,400)
+  background(0)
+  pixelDensity(displayDensity())
+  
+  ellipseMode(CENTER)
+  angleMode(DEGREES)
+  
+  
+  x1 = 0
+  y1 = 0
+  x2 = 0
+  y2 = 10
+}
+
+
+function draw(){
+  background(0, 10)
+  
+  push()
+  translate(width/2, 0)
+  
+  if (frameCount % 2 == 0) {
+    let angle = 10 * val
+    rotate(angle / (frameCount % 90))
+    val *= -1
+    
+    y1 += 10
+    y2 += 10
+  }
+  
+  if (y2 > height) {
+    y1 = 0
+    y2 = 10
+  }
+  stroke(255)
+  line(x1, y1, x2, y2)
+
+  stroke(255, 0, 0, 125)
+  
+  line(x1 + random(-10, 10), y1 + random(-10, 10), x2 + random(-10, 10), y2 + random(-10, 10))
+  pop()
+  
+}

045c06d815aed9cc8f66f04378ee0061934d357b
Fez Zaman <fzaman@oswego.edu>
2018-10-19 14:23:51 -0400

    Daily : October 17 2018

---
+++ dish.daily/p5.daily.101718/sketch.js
@@ -0,0 +1,44 @@
+// File: 101718
+
+let brain
+let brainSize
+//ATTRACTION SPEED DIVISOR FOR ALL NEURONS
+let attractionSpeed = 1000
+
+function setup(){
+  createCanvas(400,400)
+  pixelDensity(displayDensity())
+  
+  // SET BRAIN SIZE (NUMBER OF NEURONS)
+  brainSize = 10
+  
+  brain = new Array(brainSize).fill(0).map((x) => {
+    return new Neuron()
+  })
+  background(0)
+}
+
+function draw(){
+  background(0)
+  
+  if (frameCount % 90 == 0) {
+    brain = new Array(brainSize).fill(0).map((x) => {
+      return new Neuron()
+    })
+  }
+  
+  // PATCH THE BRAIN
+  
+  for (let neuron1 of brain) {
+    for (let neuron2 of brain) {
+      neuron1.patch(neuron2.output)
+    }
+  }
+  
+  // DISPLAY THE BRAIN
+  
+  for (let neuron of brain) {
+    neuron.show()
+  }
+  
+}

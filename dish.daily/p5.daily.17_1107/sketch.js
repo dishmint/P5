1c39a0c8b90c7a9fcb1bfc3421681f9458fdd173
dishmint <fzaman93@protonmail.com>
2021-09-25 21:28:29 -0400

    Several updates

--- dish.daily/p5.daily.17_1107/sketch.js
+++ dish.daily/p5.daily.17_1107/sketch.js
@@ -7,9 +7,11 @@ let deltaSpeed = 5
 
 function setup() {
 	createCanvas(400, 400);
+	// createCanvas(windowWidth / 2, windowHeight / 2);
 
 	angleMode(DEGREES)
 	radius = 50
+	noCursor()
 }
 
 function draw() {
@@ -17,26 +19,26 @@ function draw() {
 
 	// Generate base morphed circle
 	noFill()
-	translate(width/2,height/2)
+	translate(width / 2, height / 2)
 	beginShape()
 	for (let i = 0; i < 360; i++) {
-		let x = radius * cos(i) + random(-width/100, width/100)
+		let x = radius * cos(i) + random(-width / 100, width / 100)
 		let y = radius * sin(i)
 
-		stroke(255,100,0,i)
+		stroke(255, 100, 0, i)
 		if (i % 15 == 0) {
 			let j = (delta * radius) * cos(i)
-			let k = (2 * radius) * sin(i) + random(-5,1)
-			vertex(j,k)
+			let k = (2 * radius) * sin(i) + random(-5, 1)
+			vertex(j, k)
 		}
 
-		vertex(x,y)
+		vertex(x, y)
 	}
 	endShape()
 
 	delta += deltaSpeed
 
-	if(delta > 254 || delta < 0){
+	if (delta > 254 || delta < 0) {
 		deltaSpeed *= -1
 	}
 }
--- dish.daily/p5.daily.18_0905/sketch.js
+++ dish.daily/p5.daily.18_0905/sketch.js
@@ -1,24 +1,26 @@
 // File: 090518
 
-function setup(){
-  createCanvas(400,400)
-  background(0)
-  pixelDensity(displayDensity())
-  ellipseMode(CENTER)
+function setup() {
+	// createCanvas(400, 400)
+	createCanvas(windowWidth / 2, windowHeight / 2)
+	background(0)
+	pixelDensity(displayDensity())
+	ellipseMode(CENTER)
+	noCursor()
 }
 
 
-function draw(){
-  background(0)
-  
-  
-  stroke(255,0,0)
-  strokeWeight(abs(10 * cos(frameCount * 0.1)))
-  noFill()
-  push()
-  translate(width/2, height/2)
-  rotate(-frameCount)
-  ellipse(0,0, (width/2) * cos(frameCount * 0.01),(width/2))
-  pop()
-  
+function draw() {
+	background(0)
+
+
+	stroke(255, 0, 0)
+	strokeWeight(abs(10 * cos(frameCount * 0.1)))
+	noFill()
+	push()
+	translate(width / 2, height / 2)
+	rotate(-frameCount)
+	ellipse(0, 0, (width / 2) * cos(frameCount * 0.01), (width / 2))
+	pop()
+
 }
